ba650d02e45f6d29256eca92dc5c4cbc805cc8b0
SantiagoToloza <santiago.b.toloza@gmail.com>
2023-07-26 10:18:41 -0300

    Agregado mas usuarios

--- HomeBankingMindHub/Models/DbInitializer.cs
+++ HomeBankingMindHub/Models/DbInitializer.cs
@@ -11,7 +11,10 @@ namespace HomeBankingMindHub.Models
             {
                 var clients = new Client[]
                 {
-                    new Client { Email = "vcoronado@gmail.com", FirstName="Victor", LastName="Coronado", Password="123456"}
+                    new Client { Email = "vcoronado@gmail.com", FirstName="Victor", LastName="Coronado", Password="123456"},
+                    new Client { Email = "santiago.b.toloza@gmail.com", FirstName="Santiago", LastName="Toloza", Password="abcdef"},
+                    new Client { Email = "ErnestoPerez@gmail.com", FirstName="Ernesto", LastName="Perez", Password="abcdef"},
+                    new Client { Email = "abc@gmail.com", FirstName="ab", LastName="c", Password="qwerty"},
                 };
 
                 foreach (Client client in clients)
