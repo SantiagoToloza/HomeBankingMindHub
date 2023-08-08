ae16ffaa6a750fef5ecacdbcf7411416fd7b1251
SantiagoToloza <santiago.b.toloza@gmail.com>
2023-08-08 09:57:12 -0300

    Tarea 6 terminada

--- HomeBankingMindHub/Repositories/ClientRepository.cs
+++ HomeBankingMindHub/Repositories/ClientRepository.cs
@@ -33,6 +33,16 @@ namespace HomeBankingMindHub.Repositories
                 .ToList();
         }
 
+        public Client FindByEmail(string email)
+        {
+            return FindByCondition(client => client.Email.ToUpper() == email.ToUpper())
+            .Include(client => client.Accounts)
+            .Include(client => client.ClientLoans)
+                .ThenInclude(cl => cl.Loan)
+            .Include(client => client.Cards)
+            .FirstOrDefault();
+        }
+
         public void Save(Client client)
         {
             Create(client);
